90c083ca5f5ce56ef4084a4d206c31a68dfebe5b
Jon Knapp <burtonboarder8@gmail.com>
2016-03-21 09:24:40 -0700

    Hide controls for end_users.

--- app/javascripts/views/viewer.js
+++ app/javascripts/views/viewer.js
@@ -5,12 +5,13 @@ require('stylesheets/viewer.css');
 var rivets = require('rivets');
 var RTCWrapper = require('utilities/rtc_wrapper.js');
 var PresLoader = require('utilities/presentation_loader.js');
+var UserService = require('utilities/user_service.js');
 
 module.exports = Backbone.View.extend({
   template: `
     <div id="viewer" class="carousel slide" rv-if="pres.slides | length | gt 0">
       <!-- Indicators -->
-      <ol class="carousel-indicators">
+      <ol class="carousel-indicators" rv-show="user.isAdmin">
         <li rv-each-item="pres.slides" data-target="#viewer" rv-data-slide-to="index"></li>
       </ol>
 
@@ -24,11 +25,11 @@ module.exports = Backbone.View.extend({
       </div>
 
       <!-- Controls -->
-      <a class="left carousel-control" href="#viewer" role="button" data-slide="prev">
+      <a class="left carousel-control" rv-show="user.isAdmin" href="#viewer" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
-      <a class="right carousel-control" href="#viewer" role="button" data-slide="next">
+      <a class="right carousel-control" rv-show="user.isAdmin" href="#viewer" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
@@ -36,6 +37,7 @@ module.exports = Backbone.View.extend({
   `,
   events: {
     'click .carousel-control': function(e) {
+      if (!(this.scope.user.isAdmin)) return;
       var self = this;
       var dir = this.$(e.currentTarget).data('slide');
       setTimeout(function() {
@@ -44,8 +46,11 @@ module.exports = Backbone.View.extend({
         RTCWrapper.syncState();
       });
     },
-    // 'click .carousel-indicators': function(e) {
-    // },
+    'click .carousel-indicators > li': function(e) {
+      if (!(this.scope.user.isAdmin)) return;
+      RTCWrapper.state.slide = $(e.currentTarget).data('slide-to');
+      RTCWrapper.syncState();
+    },
   },
   initialize: function() {
     var self = this;
@@ -62,15 +67,17 @@ module.exports = Backbone.View.extend({
   render: function() {
     this.scope.pres = PresLoader;
     this.scope.state = RTCWrapper.state;
+    this.scope.user = UserService;
+
     this.$el.html(this.template);
     rivets.bind(this.$el, this.scope);
 
-    // Make the proper one active
+    // Make the proper slide active.
     var active = this.scope.state.slide || 0;
     this.$('.item').eq(active).addClass('active');
     this.$('.carousel-indicators > li').eq(active).addClass('active');
 
-    // Prevent autoslide
+    // Prevent autoslide.
     this.$('#viewer').carousel({ interval: false });
 
     return this;
--- dist/bundle.js
+++ dist/bundle.js
@@ -17983,12 +17983,13 @@
 	var rivets = __webpack_require__(7);
 	var RTCWrapper = __webpack_require__(18);
 	var PresLoader = __webpack_require__(21);
+	var UserService = __webpack_require__(20);
 	
 	module.exports = Backbone.View.extend({
 	  template: `
 	    <div id="viewer" class="carousel slide" rv-if="pres.slides | length | gt 0">
 	      <!-- Indicators -->
-	      <ol class="carousel-indicators">
+	      <ol class="carousel-indicators" rv-show="user.isAdmin">
 	        <li rv-each-item="pres.slides" data-target="#viewer" rv-data-slide-to="index"></li>
 	      </ol>
 	
@@ -18002,11 +18003,11 @@
 	      </div>
 	
 	      <!-- Controls -->
-	      <a class="left carousel-control" href="#viewer" role="button" data-slide="prev">
+	      <a class="left carousel-control" rv-show="user.isAdmin" href="#viewer" role="button" data-slide="prev">
 	        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 	        <span class="sr-only">Previous</span>
 	      </a>
-	      <a class="right carousel-control" href="#viewer" role="button" data-slide="next">
+	      <a class="right carousel-control" rv-show="user.isAdmin" href="#viewer" role="button" data-slide="next">
 	        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 	        <span class="sr-only">Next</span>
 	      </a>
@@ -18014,6 +18015,7 @@
 	  `,
 	  events: {
 	    'click .carousel-control': function (e) {
+	      if (!this.scope.user.isAdmin) return;
 	      var self = this;
 	      var dir = this.$(e.currentTarget).data('slide');
 	      setTimeout(function () {
@@ -18021,10 +18023,13 @@
 	        RTCWrapper.state.slide = cur;
 	        RTCWrapper.syncState();
 	      });
+	    },
+	    'click .carousel-indicators > li': function (e) {
+	      if (!this.scope.user.isAdmin) return;
+	      RTCWrapper.state.slide = $(e.currentTarget).data('slide-to');
+	      RTCWrapper.syncState();
 	    }
 	  },
-	  // 'click .carousel-indicators': function(e) {
-	  // },
 	  initialize: function () {
 	    var self = this;
 	    PresLoader.onload = function () {
@@ -18042,15 +18047,17 @@
 	  render: function () {
 	    this.scope.pres = PresLoader;
 	    this.scope.state = RTCWrapper.state;
+	    this.scope.user = UserService;
+	
 	    this.$el.html(this.template);
 	    rivets.bind(this.$el, this.scope);
 	
-	    // Make the proper one active
+	    // Make the proper slide active.
 	    var active = this.scope.state.slide || 0;
 	    this.$('.item').eq(active).addClass('active');
 	    this.$('.carousel-indicators > li').eq(active).addClass('active');
 	
-	    // Prevent autoslide
+	    // Prevent autoslide.
 	    this.$('#viewer').carousel({ interval: false });
 	
 	    return this;
