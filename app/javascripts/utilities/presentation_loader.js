c480fe8a10e41d1a6484eff5df3507f8653dac61
Jon Knapp <burtonboarder8@gmail.com>
2016-03-26 09:18:22 -0700

    Build presentation list from S3.

--- app/javascripts/utilities/presentation_loader.js
+++ app/javascripts/utilities/presentation_loader.js
@@ -2,34 +2,36 @@
 
 var AppConfig = require('app/config');
 
-var new_dummy = {
-  "folder_name1": {
-    "pres_name1": ["https://i.imgur.com/ZynM9HQ.jpg",
-                   "https://i.imgur.com/UcqFyTh.jpg"],
-    "pres_name2": ["url3", "url4"],
-  },
-  "folder_name2": {
-    "pres_name3": ["url5", "url6"],
-    "pres_name4": ["url7", "url8"],
-  }
-}
+// var listing_format_example = {
+//   "folder_name1": {
+//     "pres_name1": ["https://i.imgur.com/ZynM9HQ.jpg",
+//                    "https://i.imgur.com/UcqFyTh.jpg"],
+//     "pres_name2": ["url3", "url4"],
+//   },
+//   "folder_name2": {
+//     "pres_name3": ["url5", "url6"],
+//     "pres_name4": ["url7", "url8"],
+//   }
+// }
 
 var master_list;
+var url = window.location.protocol +'//s3.amazonaws.com/'+ AppConfig["s3_bucket"];
 
 module.exports = {
   getList: function(callback) {
-    $.ajax(window.location.protocol +'//s3.amazonaws.com/'+ AppConfig["s3_bucket"]).success(function(data) {
-      //TODO: build listing.
-      // console.log("AWS:", data.toString())
-      // console.log("XXX:", $(data).find('Contents'))
-      // console.log("JSON:", $.parseXML(data))
+    $.ajax(url).success(function(data) {
+      // build listing.
       var listing = {};
       $(data).find('Contents').each(function(i, entry) {
-        // console.log("EE",$(entry).find('Key').text())
+        var key = $(entry).find('Key').text().split('/');
+        if (key.length === 3 && key[2] !== "") {
+          var tmp = listing[key[0]]
+          if (listing[key[0]] === undefined) listing[key[0]] = {};
+          tmp = listing[key[0]];
+          if (tmp[key[1]] === undefined) tmp[key[1]] = [];
+          tmp[key[1]].push(url +'/'+ key.join('/'));
+        }
       });
-
-      listing = new_dummy;
-
       master_list = listing;
       callback.call(null, listing);
     });
